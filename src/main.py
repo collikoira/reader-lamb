38e99c784afbb5b5da702d494e86662654a10d62
antti <antti.jii3@gmail.com>
2020-05-05 23:17:44 +0300

    testing

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
 import pandas
+with open("../english/adjectives/500_adjectives") as common:
+    with open("text.txt", 'r') as dummy:
+        for words in common:
+            stripped = words.rstrip('\n')
+            for line in dummy:
+                # reading word
+                for word in line.split():
+                    if word == stripped:
+                        print(word)
+
 
